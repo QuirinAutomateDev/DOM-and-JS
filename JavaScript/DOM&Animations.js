77bc0c8c9714a1a8729012258e74a4eb0d4b98eb
Quirin DOSSOU <djihintodossou@gmail.com>
2024-09-20 23:01:08 +0300

    Mon deuxieme commit

---
+++ JavaScript/DOM&Animations.js
@@ -0,0 +1,21 @@
+
+
+let timeout 
+
+//DOM Event listener et DOM Element
+
+//Event listener with DOM element id
+function gestionnaireEvent1() {
+   timeout= setTimeout(document.getElementById('logo').src="Image/Visuel-2.jpg", 5000);
+}
+
+function gestionnaireEvent2() {
+timeout= setTimeout(document.getElementById('démo3').innerHTML= "Le mini set up n'est pas indispensable au début.", 3000);
+}
+
+
+
+document.getElementById('logo').addEventListener('mouseover', gestionnaireEvent1 );
+document.getElementById('démo3').addEventListener('mouseover', gestionnaireEvent2);
+
+
